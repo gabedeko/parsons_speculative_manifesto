0da99d9fcb368a234c7b61752725b1cea3d18f24
Gabe Deko <gabe.deko@gmail.com>
2021-11-06 16:30:34 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+new fullpage('#fullpage', {
+	//options here
+    licenseKey: '',
+	autoScrolling:true,
+	scrollHorizontally: true,
+    scrollBar: true,
+    scrollingSpeed: 600,
+    css3: true
+});
+
+//methods
+fullpage_api.setAllowScrolling(true);
