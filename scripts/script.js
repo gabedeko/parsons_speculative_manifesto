e32b501ea814cfeb2c0d87cdf61c6894c6e2be3e
Gabe Deko <gabe.deko@gmail.com>
2021-11-08 10:07:35 -0500

    add navigation

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,9 @@ console.log("hello world");
 new fullpage('#fullpage', {
 	//options here
     licenseKey: '',
+    navigation:true,
+    navigationTooltips: ['Intro', '1800s', '1890', '1946', '1950s-70s', '1970s-80s', '1980s-Present', 'Present', 'Takeaway', 'Work Cited'],
+    showActiveTooltip: true,
 	autoScrolling:true,
 	scrollHorizontally: true,
     scrollBar: true,
